1664eb18683392984fa23bd43f560239e8c0f723
jreitz2 <jreitz2@gmail.com>
2023-01-17 11:01:19 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # restaurant
+
+This project uses webpack. The restaurant site's content is genrated with js.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css'
+// import each tab module here
+
+console.log(1235);
+
+// enter the code for tab switching here
